d2edb80884a7ae4e9b9593e8cf17fed21c976d96
Borhan <63269814+ularif@users.noreply.github.com>
2023-03-27 19:46:48 +0600

    Modifying MyClass of newbranch

--- app/src/main/java/MyClass.java
+++ app/src/main/java/MyClass.java
@@ -2,7 +2,7 @@ public class MyClass {
     private int myField;
 
     public MyClass(int myField) {
-        this.myField = myField;
+        this.myField = myField; //this is a constructor
     }
 
     public int getMyField() {
